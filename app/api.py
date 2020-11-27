f16b5470656c5af8b7d5ba305236bebae80d387f
Yinghua Mo <clairemo977@gmail.com>
2020-11-26 20:37:43 -0600

    confirmed APIs

--- app/api.py
+++ app/api.py
@@ -678,191 +678,191 @@ def shopping_cart_delete_item_from_user_shopping_cart():
 
 
 
-# TODO
-@app.route('/admin-user-query', methods=['POST'])
-def admin_user_query():
-    """
-    admin-user-query API
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          id: admin-user-query
-          properties:
-            keyword:
-              type: string
-    responses:
-      200:
-        description: Succeeded - search result returned.
-      400:
-        description: Failed - fail to search.
-    """
-    keyword = request.json.get('keyword')
-    with ClusterRpcProxy(CONFIG) as rpc:
-        search_result, result, msg = rpc.admin.search_user(keyword)
-    if result:
-        return msg, 200
-    return msg, 400
+# # TODO
+# @app.route('/admin-user-query', methods=['POST'])
+# def admin_user_query():
+#     """
+#     admin-user-query API
+#     ---
+#     parameters:
+#       - name: body
+#         in: body
+#         required: true
+#         schema:
+#           id: admin-user-query
+#           properties:
+#             keyword:
+#               type: string
+#     responses:
+#       200:
+#         description: Succeeded - search result returned.
+#       400:
+#         description: Failed - fail to search.
+#     """
+#     keyword = request.json.get('keyword')
+#     with ClusterRpcProxy(CONFIG) as rpc:
+#         search_result, result, msg = rpc.admin.search_user(keyword)
+#     if result:
+#         return msg, 200
+#     return msg, 400
 
 
 
 
 
-# TODO
-@app.route('/login-admin-login', methods=['POST'])
-def login_admin_login():
-    """
-    login-admin-login API
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          id: login-admin-login
-          properties:
-            admin:
-              type: string
-            password:
-              type: string
-    responses:
-      200:
-        description: Succeeded - admin logged in.
-      400:
-        description: Failed - admin failed to login.
-    """
-    admin = request.json.get('admin')
-    password = request.json.get('password')
-    with ClusterRpcProxy(CONFIG) as rpc:
-        result, msg = rpc.login.admin_login(admin, password)
-    if result:
-        return msg, 200
-    return msg, 400
+# # TODO
+# @app.route('/login-admin-login', methods=['POST'])
+# def login_admin_login():
+#     """
+#     login-admin-login API
+#     ---
+#     parameters:
+#       - name: body
+#         in: body
+#         required: true
+#         schema:
+#           id: login-admin-login
+#           properties:
+#             admin:
+#               type: string
+#             password:
+#               type: string
+#     responses:
+#       200:
+#         description: Succeeded - admin logged in.
+#       400:
+#         description: Failed - admin failed to login.
+#     """
+#     admin = request.json.get('admin')
+#     password = request.json.get('password')
+#     with ClusterRpcProxy(CONFIG) as rpc:
+#         result, msg = rpc.login.admin_login(admin, password)
+#     if result:
+#         return msg, 200
+#     return msg, 400
 
 
-@app.route('/item-auction-list', methods=['POST'])
-def item_auction_list():
-    """
-    item-auction-list API
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          id: item-auction-list
-          properties:
-            status:
-              type: string
-    responses:
-      200:
-        description: Succeeded - user succeeded to get item list.
-      400:
-        description: Failed - user failed to get item list..
-    """
-    status = request.json.get('status')
-    with ClusterRpcProxy(CONFIG) as rpc:
-        result, data = rpc.auction.list_item(status)
-    if result:
-        return jsonify(data), 200
-    return {}, 400
+# @app.route('/item-auction-list', methods=['POST'])
+# def item_auction_list():
+#     """
+#     item-auction-list API
+#     ---
+#     parameters:
+#       - name: body
+#         in: body
+#         required: true
+#         schema:
+#           id: item-auction-list
+#           properties:
+#             status:
+#               type: string
+#     responses:
+#       200:
+#         description: Succeeded - user succeeded to get item list.
+#       400:
+#         description: Failed - user failed to get item list..
+#     """
+#     status = request.json.get('status')
+#     with ClusterRpcProxy(CONFIG) as rpc:
+#         result, data = rpc.auction.list_item(status)
+#     if result:
+#         return jsonify(data), 200
+#     return {}, 400
 
-@app.route('/item-auction-update', methods=['POST'])
-def item_auction_update():
-    """
-    item-auction-update API
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          id: item-auction-update
-          properties:
-            item_id:
-              type: string
-    responses:
-      200:
-        description: Succeeded - auction status is updated.
-      400:
-        description: Failed - no need to update auction status.
-    """
-    item_id = request.json.get('item_id')
-    with ClusterRpcProxy(CONFIG) as rpc:
-        result, status = rpc.auction.update_auction_status(item_id)
-    if result:
-        return status, 200
-    return status, 400
-
-
-@app.route('/item-auction-set-window', methods=['POST'])
-def item_auction_set_window():
-    """
-    item-auction-set-window API
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          id: item-auction-set-window
-          properties:
-            item_id:
-              type: string
-            start_time:
-              type: integer
-            end_time:
-              type: integer
-    responses:
-      200:
-        description: Succeeded - set auction window.
-      400:
-        description: Failed - unable to set auction window.
-    """
-    item_id = request.json.get('item_id')
-    start_time = request.json.get('start_time')
-    end_time = request.json.get('end_time')
-    with ClusterRpcProxy(CONFIG) as rpc:
-        result, msg = rpc.auction.set_auction_window(item_id, start_time, end_time)
-    if result:
-        return msg, 200
-    return msg, 400
+# @app.route('/item-auction-update', methods=['POST'])
+# def item_auction_update():
+#     """
+#     item-auction-update API
+#     ---
+#     parameters:
+#       - name: body
+#         in: body
+#         required: true
+#         schema:
+#           id: item-auction-update
+#           properties:
+#             item_id:
+#               type: string
+#     responses:
+#       200:
+#         description: Succeeded - auction status is updated.
+#       400:
+#         description: Failed - no need to update auction status.
+#     """
+#     item_id = request.json.get('item_id')
+#     with ClusterRpcProxy(CONFIG) as rpc:
+#         result, status = rpc.auction.update_auction_status(item_id)
+#     if result:
+#         return status, 200
+#     return status, 400
 
 
-@app.route('/notification-send-email', methods=['POST'])
-def notification_send_email():
-    """
-    notification-send-email API
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          id: notification-send-email
-          properties:
-            email_address:
-              type: string
-            subject:
-              type: string
-            content:
-              type: string
-    responses:
-      200:
-        description: Succeeded.
-      400:
-        description: Failed.
-    """
-    email_address = request.json.get('email_address')
-    subject = request.json.get('subject')
-    content = request.json.get('content')
-    with ClusterRpcProxy(CONFIG) as rpc:
-        result, msg = rpc.notification.send_email(email_address, subject, content)
-    if result:
-        return msg, 200
-    return msg, 400
+# @app.route('/item-auction-set-window', methods=['POST'])
+# def item_auction_set_window():
+#     """
+#     item-auction-set-window API
+#     ---
+#     parameters:
+#       - name: body
+#         in: body
+#         required: true
+#         schema:
+#           id: item-auction-set-window
+#           properties:
+#             item_id:
+#               type: string
+#             start_time:
+#               type: integer
+#             end_time:
+#               type: integer
+#     responses:
+#       200:
+#         description: Succeeded - set auction window.
+#       400:
+#         description: Failed - unable to set auction window.
+#     """
+#     item_id = request.json.get('item_id')
+#     start_time = request.json.get('start_time')
+#     end_time = request.json.get('end_time')
+#     with ClusterRpcProxy(CONFIG) as rpc:
+#         result, msg = rpc.auction.set_auction_window(item_id, start_time, end_time)
+#     if result:
+#         return msg, 200
+#     return msg, 400
+
+
+# @app.route('/notification-send-email', methods=['POST'])
+# def notification_send_email():
+#     """
+#     notification-send-email API
+#     ---
+#     parameters:
+#       - name: body
+#         in: body
+#         required: true
+#         schema:
+#           id: notification-send-email
+#           properties:
+#             email_address:
+#               type: string
+#             subject:
+#               type: string
+#             content:
+#               type: string
+#     responses:
+#       200:
+#         description: Succeeded.
+#       400:
+#         description: Failed.
+#     """
+#     email_address = request.json.get('email_address')
+#     subject = request.json.get('subject')
+#     content = request.json.get('content')
+#     with ClusterRpcProxy(CONFIG) as rpc:
+#         result, msg = rpc.notification.send_email(email_address, subject, content)
+#     if result:
+#         return msg, 200
+#     return msg, 400
 
 
 
